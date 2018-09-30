0500d0f471b82009640750f121409e4c6149acfe
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-30 23:17:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
-  
+
 
   def initialize
     @@count += 1
@@ -18,7 +18,7 @@ class Song
   def name=(new_value)
     @name = new_value
   end
-  
+
   def count
     @@count
   end
