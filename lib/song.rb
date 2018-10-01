d8efda8f64618510f740db4d821b7ebeeb47d5b4
Susan Rolfes <rolfes57069@yahoo.com>
2018-10-01 00:45:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,10 @@ class Song
   @@genre_count = {}
   @@artist_count = {}
 
-  def initialize (name, artist, genre)
+  def initialize(name, artist, genre)
     @@name = name
-
+    @@artist = artist
+    @@genre = genre
     @@count += 1
     @@genres << @genre
   end
